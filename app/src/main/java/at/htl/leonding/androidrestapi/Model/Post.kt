5f0786230007ea8e3727513d976a63793f10eba3
David <unbekannt.ghost@gmail.com>
2022-02-14 11:34:33 +0100

    id string to int

--- app/src/main/java/at/htl/leonding/androidrestapi/Model/Post.kt
+++ app/src/main/java/at/htl/leonding/androidrestapi/Model/Post.kt
@@ -3,6 +3,6 @@ package at.htl.leonding.androidrestapi.Model
 import android.os.Build
 import androidx.annotation.RequiresApi
 import java.util.*
-@RequiresApi(Build.VERSION_CODES.O)
-data class Post(val id: String, val date: String, val title: Rendered, val content: Rendered) {
+//@RequiresApi(Build.VERSION_CODES.O)
+data class Post(val id: Int, val date: String, val title: Rendered, val content: Rendered) {
 }
