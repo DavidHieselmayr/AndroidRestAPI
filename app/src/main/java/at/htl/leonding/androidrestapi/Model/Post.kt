4fd57628e7aa6fd4899b675541756478a36aec65
David <unbekannt.ghost@gmail.com>
2022-02-14 09:39:42 +0100

    add basic

---
+++ app/src/main/java/at/htl/leonding/androidrestapi/Model/Post.kt
@@ -0,0 +1,6 @@
+package at.htl.leonding.androidrestapi.Model
+
+import java.util.*
+
+class Post(val id: Number, val date: Date, val title: Rendered, val content: Rendered) {
+}
---
+++ app/src/main/java/at/htl/leonding/androidrestapi/Model/Rendered.kt
@@ -0,0 +1,4 @@
+package at.htl.leonding.androidrestapi.Model
+
+class Rendered(val content: String) {
+}
