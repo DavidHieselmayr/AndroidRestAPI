2468611dd8d23132c7bf61aeba981e1fac644966
David <unbekannt.ghost@gmail.com>
2022-02-28 17:26:09 +0100

    remove old project

--- app/src/main/java/at/htl/leonding/androidrestapi/Model/Post.kt
+++
@@ -1,8 +0,0 @@
-package at.htl.leonding.androidrestapi.Model
-
-import android.os.Build
-import androidx.annotation.RequiresApi
-import java.util.*
-//@RequiresApi(Build.VERSION_CODES.O)
-data class Post(val id: Int, val date: String, val title: Rendered, val content: Rendered) {
-}
--- app/src/main/java/at/htl/leonding/androidrestapi/Model/Rendered.kt
+++
@@ -1,4 +0,0 @@
-package at.htl.leonding.androidrestapi.Model
-
-data class Rendered(val rendered: String) {
-}
